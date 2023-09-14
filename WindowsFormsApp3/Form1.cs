48564c1679c7d197518b1c6490e7123ad8d3b36d
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-09-14 17:06:21 +0200

    Add files via upload

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Zamestnanec zamestnanec;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            string titul = "Ing.";
+            string jmeno = "Ondřej Zelený";
+            int mesicniplat = 50000;
+            DateTime datumnastupu = dateTimePicker1.Value;
+            int ohodnoceni = 1000;
+            int odpracovanehodiny = 10;
+            zamestnanec = new Zamestnanec(titul, jmeno, mesicniplat, datumnastupu, ohodnoceni, odpracovanehodiny);
+            MessageBox.Show(zamestnanec.ToString(), "Info");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            zamestnanec.ZvysOhodnoceni();
+            MessageBox.Show(zamestnanec.ToString(), "Info");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            zamestnanec.PridejOdpracovaneHodiny();
+            MessageBox.Show(zamestnanec.ToString(), "Info");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            zamestnanec.Relaxace((int)numericUpDown1.Value);
+            MessageBox.Show(zamestnanec.ToString(), "Info");
+        }
+    }
+}
---
+++ WindowsFormsApp3/Zamestnanec.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Zamestnanec
+    {
+        string titul;
+        string jmeno;
+        int mesicniplat;
+        DateTime datumnastupu;
+        int ohodnoceni;
+        int odpracovanehodiny;
+        string pozice = "pracovník";
+        public Zamestnanec(string titul, string jmeno, int mesicniplat, DateTime datumnastupu, int ohodnoceni, int odpracovanehodiny)
+        {
+            this.titul = titul;
+            this.jmeno = jmeno;
+            this.mesicniplat = mesicniplat;
+            this.datumnastupu = datumnastupu;
+            this.ohodnoceni = 0;
+            this.odpracovanehodiny = 0;
+        }
+        public string Jmeno
+        {
+            get
+            {
+                if (jmeno[0] < 'A' || jmeno[0] > 'Z')
+                {
+                    jmeno = (char)(jmeno[0] + 32) + jmeno.Substring(1);
+                }
+                string[] jmenoaprijmeni = jmeno.Split(' ');
+                jmenoaprijmeni[1] = jmenoaprijmeni[1].ToUpper();
+                return jmenoaprijmeni[0] + " " + jmenoaprijmeni[1];
+            }
+            set
+            {
+                jmeno = value;
+
+            }
+        }
+        public string Titul
+        {
+            get
+            {
+                if (titul[0] < 'A' || titul[0] > 'Z')
+                {
+                    titul = (char)(titul[0] + 32) + titul.Substring(1) + '.';
+                }
+                return titul;
+            }
+            set
+            {
+                titul = value;
+            }
+        }
+        public int Rocniplat()
+        {
+            return 12 * mesicniplat + ohodnoceni;
+        }
+        public int Pocetmesicu()
+        {
+            TimeSpan cc = new TimeSpan();
+            cc = DateTime.Now - datumnastupu;
+            return (int)cc.TotalDays / 30;
+        }
+        public int Celkem()
+        {
+            return Pocetmesicu() * mesicniplat;
+        }
+        public void ZvysOhodnoceni()
+        {
+            ohodnoceni += 100;
+            if (ohodnoceni > 10000)
+            {
+                pozice = "vedoucí";
+            }
+        }
+        public int PridejOdpracovaneHodiny()
+        {
+            odpracovanehodiny++;           
+            return odpracovanehodiny;
+        }
+        public int Relaxace(int hodinrelaxace)
+        {
+            return hodinrelaxace;
+        }
+        public override string ToString()
+        {
+            return "Titul: " + titul + "\nJméno: " + jmeno + "\nMěsíční plat: " + mesicniplat + "\nPočet měsíců v práci: " + Pocetmesicu() + "\nOsobní ohodnoceni: " + ohodnoceni + "\nRoční plat: " + Rocniplat() + "\nCelkový plat: " + Celkem() + "\nPočet odpracovaných hodin: " + odpracovanehodiny + "\nPozice: " + pozice;
+        }
+    }
+}
